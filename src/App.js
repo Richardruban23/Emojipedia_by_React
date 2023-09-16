e42b31d6d021c59de00ca4e26878648d10346fe1
Ruban_2309 <145167225+Richardruban23@users.noreply.github.com>
2023-09-16 20:21:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./index.css";
+import Card from "./Card";
+import emojipedia from "./emoji";
+
+function createCard(props) {
+  return (
+    <Card
+      key={props.id}
+      emoji={props.emoji}
+      name={props.name}
+      meaning={props.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
